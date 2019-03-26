e9fdd1e418c506b223b8260bc00eb0f08aebb98f
Sven Weinschenk <svewei6@gmail.com>
2019-03-26 21:41:48 +0100

    Dateien hinzugefügt.

--- Wheniskeynote/ViewController.swift
+++ Wheniskeynote/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mWebKit: WKWebView!
+    let urlMy = URL(string: "http://wheniskeynote.com/")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let request = URLRequest(url: urlMy!)
+        mWebKit.load(request)
     }
 
 
+    
 }
 
